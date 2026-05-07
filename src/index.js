d9e6307de137f575dda407b3793c0f03ee5821e6
Olivia Mackintosh <livvy@base.nu>
2026-05-07 20:56:58 +0100

    debug: redirect stderr to stdout

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import process from 'node:process'
 import { startServer } from '#/server/common/helpers/start-server.js'
 import { createLogger } from '#/server/common/helpers/logging/logger.js'
 
+process.stderr.write = (() => {
+  const write = process.stdout.write.bind(process.stdout);
+  return (chunk, encoding, callback) => write(chunk, encoding, callback);
+})();
+
 await startServer()
 
 process.on('unhandledRejection', (error) => {
