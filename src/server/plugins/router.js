0b8265a8ee672ef39935b5d7ab8ef4a63961cc4c
Olivia Mackintosh <liv@base.nu>
2026-06-02 09:20:41 +0100

    fix: override hapi-connnect plugin to set auth to false
    Static files served by vite need auth turned off but @defra/hapi-connect
    does not allow this...

--- src/server/plugins/router.js
+++ src/server/plugins/router.js
@@ -6,6 +6,46 @@ import { check } from '../routes/check/index.js'
 import { health } from '../routes/health/index.js'
 import { serveStaticFiles } from './serve-static-files.js'
 import { config } from '#/config/config.js'
+import { notFound } from '@hapi/boom'
+import { finished } from 'node:stream/promises'
+import connect from 'connect'
+
+const hapiconnect = {
+  name: 'connect',
+  version: '0.1.0',
+  description: 'Mount `connect` based middleware at a route',
+  register: async function (server, options) {
+    const { path, middleware, auth } = options
+
+    const app = connect()
+
+    server.route({
+      method: '*',
+      path: `${path}/{param*}`,
+      options: {
+        auth
+      },
+      handler: async (request, h) => {
+        for (const item of middleware) {
+          app.use(path, item)
+        }
+
+        const { req, res } = request.raw
+
+        const { promise: next, resolve: resolveNext } = Promise.withResolvers()
+        app(req, res, () => resolveNext(true))
+
+        const nextCalled = await Promise.race([finished(res), next])
+
+        if (nextCalled) {
+          return notFound()
+        }
+
+        return h.abandon
+      }
+    })
+  }
+}
 
 export const router = {
   plugin: {
@@ -30,10 +70,11 @@ export const router = {
           })
 
           await server.register({
-            plugin: (await import('@defra/hapi-connect')).default,
+            plugin: hapiconnect,
             options: {
               path: '/public',
               middleware: [vite.middlewares],
+              auth: false
             },
           })
         })()
